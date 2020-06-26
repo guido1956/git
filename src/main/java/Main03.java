0257f26b847525a055ec2e7c879aeebcddcf3ca6
guido1956 <49561414+guido1956@users.noreply.github.com>
2020-06-26 19:48:24 +0200

    Versie 0.03test

--- src/main/java/Main03.java
+++ src/main/java/Main03.java
@@ -1,6 +1,7 @@
 public class Main03 {
     public static void main(String[] args) {
         Boeken mijnVerzameling = new Boeken();
+       mijnVerzameling.addBoek(new Boek("Hamlet", "W. Shakespeare"));
 
     }
 }
