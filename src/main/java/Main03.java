24f5468e99f8f519d7165ebbf518c33394b3d7f2
guido1956 <49561414+guido1956@users.noreply.github.com>
2020-06-26 19:49:46 +0200

    Versie 0.03test

--- src/main/java/Main03.java
+++ src/main/java/Main03.java
@@ -1,7 +1,7 @@
 public class Main03 {
     public static void main(String[] args) {
         Boeken mijnVerzameling = new Boeken();
-       mijnVerzameling.addBoek(new Boek("Hamlet", "W. Shakespeare"));
-
+        mijnVerzameling.addBoek(new Boek("Hamlet", "W. Shakespeare"));
+        mijnVerzameling.addBoek(new Boek("The Tempest", "W. Shakespeare"));
     }
 }
