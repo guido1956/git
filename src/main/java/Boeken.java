457e96cf486b9f1aaa3939119eb2bdc3f835e22b
guido1956 <49561414+guido1956@users.noreply.github.com>
2020-06-26 19:37:57 +0200

    Versie 0.03

---
+++ src/main/java/Boeken.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Boeken {
+    private ArrayList<Boek> boeken = new ArrayList<>();
+
+    public Boeken() {
+    }
+
+    public void addBoek(Boek boek) {
+        boeken.add(boek);
+    }
+
+
+
+}
